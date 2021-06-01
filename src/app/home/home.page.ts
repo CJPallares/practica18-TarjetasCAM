49773496c0b276e0302f4dfca521a11570b30bf7
CJPallares <cj.j.pallares@gmail.com>
2021-06-01 22:00:31 +0200

    fixed ccv

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,52 +4,53 @@ import { NavigationExtras } from '@angular/router';
 import { NavController } from '@ionic/angular';
 
 @Component({
-selector: 'app-home',
-templateUrl: 'home.page.html',
-styleUrls: ['home.page.scss'],
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
 
 })
-export class HomePage implements OnInit{
+export class HomePage implements OnInit {
 
-    formulario: FormGroup;
-    tiposTarjeta : Array<string>;
+  formulario: FormGroup;
+  tiposTarjeta: Array<string>;
 
-constructor(
+  constructor(
     public formBuilder: FormBuilder,
     private navCtrl: NavController
-) {}
+  ) { }
 
-ngOnInit() {
+  ngOnInit() {
 
     this.tiposTarjeta = [
-        "Mastercard",
-        "American Express",
-        "Discover",
-        "Visa"
-        ];
+      "Mastercard",
+      "American Express",
+      "Discover",
+      "Visa"
+    ];
 
 
-this.formulario = this.formBuilder.group({    
-    numTarjeta: new FormControl('', Validators.compose([
+    this.formulario = this.formBuilder.group({
+      numTarjeta: new FormControl('', Validators.compose([
         Validators.minLength(16),
         Validators.pattern('^[0-9]*$'),
         Validators.required
       ])),
-    expira : new FormControl('', Validators.compose([
+      expira: new FormControl('', Validators.compose([
         Validators.pattern('^(0[1-9]|1[0-2])\/([0-9]{2})$'),
         Validators.required
-    ])),
-    ccv: new FormControl('', Validators.compose([
+      ])),
+      ccv: new FormControl('', Validators.compose([
         Validators.minLength(3),
+        Validators.pattern('^[0-9]*$'),
         Validators.required
-    ])),
-    tipo: new FormControl('', Validators.required),
-    clientePromocional : new FormControl('' , Validators.required),
-    tipoTarjeta: new FormControl(this.tiposTarjeta[0], Validators.required) //estaba comentado ¿?
-});
-}
+      ])),
+      tipo: new FormControl('', Validators.required),
+      clientePromocional: new FormControl('', Validators.required),
+      tipoTarjeta: new FormControl(this.tiposTarjeta[0], Validators.required) //estaba comentado ¿?
+    });
+  }
 
-onSubmit(values){
+  onSubmit(values) {
     console.log(values);
     let navigationExtras: NavigationExtras = {
       queryParams: {
@@ -59,7 +60,7 @@ onSubmit(values){
     this.navCtrl.navigateForward('/pagina2', navigationExtras);
   }
 
-  clickBorrar(){
+  clickBorrar() {
     this.formulario.controls['numTarjeta'].setValue('');
     this.formulario.controls['expira'].setValue('');
     this.formulario.controls['ccv'].setValue('');
@@ -67,5 +68,5 @@ onSubmit(values){
     this.formulario.controls['tipo'].setValue('');
     this.formulario.controls['clientePromocional'].setValue(false);
   }
-  
+
 }//final de clase
